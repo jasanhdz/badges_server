370f8043debe8277ddf1f0c53aba659985aef800
Jasan Hernandez <jasan_azael@hotmail.es>
2020-03-03 17:44:19 -0600

    adding middlewares to the express app

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const cors = require("cors");
 const badges = require("./routes/badges");
 const { config } = require("./config");
 
+const {
+  logErrors,
+  wrapErrors,
+  errorHandler
+} = require("./utils/middleware/errorHandler");
+
 const app = express();
 
 app.use(express.json());
@@ -10,6 +16,13 @@ app.use(cors());
 
 // routes
 badges(app);
+
+// Los middlewares de error, siempre tienen que ir al final de las rutas,
+// las rutas también son middlewares
+// **Menejadores de errores**
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
   res.send("Hello word a API Bages Platzi");
